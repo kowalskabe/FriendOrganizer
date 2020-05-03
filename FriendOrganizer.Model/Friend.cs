92ca331c0fce6ed7142fad50a44708b22573dbf4
kowalskabe <58075828+kowalskabe@users.noreply.github.com>
2020-05-03 18:47:30 +0200

    Adding DataAccess and Model to solution.  Setting the references. Creating Friend class to FriendOrganization.Model with basic properties.

---
+++ FriendOrganizer.Model/Friend.cs
@@ -0,0 +1,10 @@
+﻿namespace FriendOrganizer.Model
+{
+    public class Friend
+    {
+        public int Id { get; set; }
+        public int FirstName { get; set; }
+        public int LastName { get; set; }
+        public int Email { get; set; }
+    }
+}
